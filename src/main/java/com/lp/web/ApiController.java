f9c895f82aeb5d1c1a1c6723d2a4137b8d952858
evgeny baranov <evgeny@gmail.com>
2023-12-12 02:33:33 +0200

    gcp mysql added

--- src/main/java/com/lp/web/ApiController.java
+++ src/main/java/com/lp/web/ApiController.java
@@ -1,5 +1,7 @@
 package com.lp.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+    @Value("sm://test-secret")
+    private String secret;
+
+    @GetMapping("/test")
+    public String getTest() {
+        return this.secret;
+    }
 }
